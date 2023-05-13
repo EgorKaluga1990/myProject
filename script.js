eab5aab933c15e276fe03020d1c491db105e8eae
Egor <111>
2023-05-13 12:09:44 +0300

    second 2

--- script.js
+++ script.js
@@ -144,3 +144,132 @@ console.log(isChecked || !isClose);
 
 console.log(2 + 2 * 4 !== '6');//true
 
+//Условия
+
+if (4 == 4) {
+  console.log('ok')
+} else {
+  console.log('error')
+}
+
+const pal = 50;
+
+// if (num < 49) {
+//   console.log('error');
+// } else if (num > 100) {
+//   console.log('many')
+// } else {
+//   console.log('ok')
+// }
+
+// (num === 50) ? console.log('ok') : console.log('error'); //Тернарный оператор
+
+switch (pal) {
+  case 49:
+    console.log('neverno');
+    break;
+  case 100:
+    console.log('neverno');
+    break;
+  case 50:
+    console.log('pravda');
+    break;
+  default:
+    console.log('ne v etot raz');
+    break;
+}
+
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//   console.log('nice!');
+// }
+
+// console.log((hamburger && fries));
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'sdffhs');
+
+// if (hamburger === 3 && cola && fries) {
+//   console.log('nice!');
+// } else {
+//   console.log('bad');
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+  console.log('nice!');
+} else {
+  console.log('bad');
+}
+
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+let JonReport, alexReport, samReport, mariaReport = 'done';
+console.log(JonReport || alexReport || samReport || mariaReport);
+
+console.log(!0); //оператор НЕ
+
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+//ЦИКЛЫ
+
+let num = 50;
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while(num <= 55);
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 6) {
+    // break;
+    continue;
+  }
+  console.log(i);
+}
+
+
